d9165e269983956a34d9b8b7db5df9dbcd9ef17e
Ahsan Tariq <ahsantariq0724@gmail.com>
2024-01-07 16:18:24 +0500

    some updates

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='isgc',
-    version='0.1.1',
+    version='0.1.2',
     author='Ahsan Tariq',
     author_email='ahsantariq0724@gmail.com',
     description='A SGPA and CGPA Calculator for Islamia University Bahawalpur Graduation Students under supervision of Malik Shahzad, a Senior Developer. Also thankful for departmental support from M. Bux Alvi Sahb and Engr. Mubashir Hussain. A motivation from Muhammad Ibrar.Also very thankful to Sundas Tariq and Laiba Saleem for contributing to this project and making it usable for all IUB Graduation Students. Some friendly support from Asim Zubair and Usman Akram',
